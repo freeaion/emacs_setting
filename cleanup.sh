b5dbb5eed0fda246aa954c72251304be5b4554e4
freeaion <freeaion@gmail.com>
2023-03-07 20:11:53 -0800

    remove helm-config

--- cleanup.sh
+++ cleanup.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-\rm -fr auto-save-list  elpa  transient  abbrev_defs  dired-history  my_settings.el .emacs.desktop .emacs.desktop.lock projectile-bookmarks.eld undo recentf recently.el .persistent-scratch bm-repository .lsp* .cache url
+\rm -fr auto-save-list  elpa  transient  abbrev_defs  dired-history  my_settings.el .emacs.desktop .emacs.desktop.lock projectile-bookmarks.eld undo recentf recently.el .persistent-scratch bm-repository .lsp* .cache url snippets
