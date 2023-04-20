3098315885127a4b716b3288c42f3f1fb2d670af
Yonghyun Hwang <freeaion@gmail.com>
2023-04-19 21:48:15 -0700

    add eln-cache & tramp for cleanup

--- cleanup.sh
+++ cleanup.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-\rm -fr auto-save-list  elpa  transient  abbrev_defs  dired-history  my_settings.el .emacs.desktop .emacs.desktop.lock projectile-bookmarks.eld undo recentf recently.el .persistent-scratch bm-repository .lsp* .cache url snippets .mc-lists.el
+\rm -fr auto-save-list  elpa  transient  abbrev_defs  dired-history  my_settings.el .emacs.desktop .emacs.desktop.lock projectile-bookmarks.eld undo recentf recently.el .persistent-scratch bm-repository .lsp* .cache url snippets .mc-lists.el eln-cache tramp
 git checkout init.el
 
