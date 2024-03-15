94774257cf5797c536c8410d26d45edd3d29412c
Yonghyun Hwang <freeaion@gmail.com>
2024-03-15 15:16:02 -0700

    cleanup to use -ts-mode

--- cleanup.sh
+++ cleanup.sh
@@ -1,5 +1,28 @@
 #!/usr/bin/env bash
 
-\rm -fr auto-save-list  elpa  transient  abbrev_defs  dired-history  my_settings.el .emacs.desktop .emacs.desktop.lock projectile-bookmarks.eld undo recentf recently.el .persistent-scratch bm-repository .lsp* .cache url snippets .mc-lists.el eln-cache tramp persp-confs async-bytecomp.log
+\rm -fr \
+    .cache \
+    .emacs.desktop .emacs.desktop.lock \
+    .lsp* \
+    .mc-lists.el \
+    .persistent-scratch \
+    abbrev_defs \
+    async-bytecomp.log \
+    auto-save-list \
+    bm-repository \
+    dired-history \
+    eln-cache \
+    elpa \
+    my_settings.el \
+    persp-confs \
+    projectile-bookmarks.eld \
+    recentf \
+    recently.el\
+    snippets \
+    tramp \
+    transient \
+    undo \
+    url
+
 git checkout init.el
 
