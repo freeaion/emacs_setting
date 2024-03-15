6bf1fd21ac0f998e9191a581c243d19eaf33dbe9
Yonghyun Hwang <freeaion@gmail.com>
2024-03-15 13:49:16 -0700

    automate git repo create

--- github.sh
+++ github.sh
@@ -1,4 +1,26 @@
 #!/usr/bin/env bash
-ssh-keygen -t rsa -b 4096 -C "freeaion@gmail.com"
+
+if [[ $# < 1 ]]; then
+    echo "specify path to clone git repo for emacs"
+    echo "\$ ${0} \$PATH_TO_CLONE"
+    exit 1
+fi
+
+declare -r CLONE_PATH="${1}"
+
+if [ -e "${CLONE_PATH}" ]; then
+    echo "${CLONE_PATH} already exists"
+    exit 1
+fi
+
+mkdir -p "${CLONE_PATH}" && cd "${CLONE_PATH}"
+
+git init
+# override ssh command for private key
+git config core.sshCommand "ssh -i ~/.ssh/id_ed25519_freeaion"
+
+# add remote repo
 git remote add origin git@github.com:freeaion/emacs_setting.git
-git push -u origin master
+
+# pull!
+git pull origin master
