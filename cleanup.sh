5b767cdda5cac84b9d0eb57f0897bae4858e3313
Yonghyun Hwang <freeaion@gmail.com>
2024-03-15 13:08:03 -0700

    add presp-confs for cleanup

--- cleanup.sh
+++ cleanup.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-\rm -fr auto-save-list  elpa  transient  abbrev_defs  dired-history  my_settings.el .emacs.desktop .emacs.desktop.lock projectile-bookmarks.eld undo recentf recently.el .persistent-scratch bm-repository .lsp* .cache url snippets .mc-lists.el eln-cache tramp
+\rm -fr auto-save-list  elpa  transient  abbrev_defs  dired-history  my_settings.el .emacs.desktop .emacs.desktop.lock projectile-bookmarks.eld undo recentf recently.el .persistent-scratch bm-repository .lsp* .cache url snippets .mc-lists.el eln-cache tramp persp-confs
 git checkout init.el
 
